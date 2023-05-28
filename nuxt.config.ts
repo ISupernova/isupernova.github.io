213d12d1b4dd4b3bed6d9a3cd719aacbc461243e
Abdullah <habar@protonmail.com>
2023-05-28 11:07:47 +0200

    Add eslint and prettir config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,23 @@
 export default defineNuxtConfig({
-  app: {},
-  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: "Gazelle",
+      htmlAttrs: {
+        lang: "en"
+      },
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" }
+      ],
+      noscript: [
+        { children: "Javascript is required" }
+      ]
+    }
+  },
+  modules: ["@nuxtjs/tailwindcss"],
   css: [
-    '@fortawesome/fontawesome-svg-core/styles.css'
+    "@fortawesome/fontawesome-svg-core/styles.css"
   ],
   build: {
-    transpile: ['@fortawesome/vue-fontawesome']
+    transpile: ["@fortawesome/vue-fontawesome"]
   },
 })
