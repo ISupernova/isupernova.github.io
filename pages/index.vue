60162f15a032b7b4680a1532ffc1eca7d6d8fe49
Abdullah <habar@protonmail.com>
2023-05-28 11:07:47 +0200

    Homepage updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,44 @@
 <template>
+  <div
+    class="h-screen bg-fixed bg-center bg-cover custom-img"
+  >
+  <Navbar />
   <main>
-    <div>
-      <h1>Home</h1>
-      <NuxtLink to="/safe-gallery">Safe gallery</NuxtLink>
+    <div
+      class="relative pt-16 pb-32 flex content-center items-center justify-center"
+      style="min-height: 75vh"
+    >
+      <section class="relative block py-24 bg-green-900">
+        <div class="container relative mx-auto">
+          <div class="items-center flex flex-wrap">
+            <div class="w-full px-4 ml-auto mr-auto text-center">
+              <div class="px-12">
+                <h1 class="text-white font-semibold text-5xl">
+                  Choose the gallery
+                </h1>
+                <p class="mt-4 text-lg text-gray-300">
+                  Choose your gallery visibility level
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   </main>
+  </div>
 </template>
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+<style scoped>
+.custom-img {
+  background-image: url("https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1955&q=80");
+}
+</style>
