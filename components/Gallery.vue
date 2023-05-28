0052ce3a2a90278c24f15afd0f2d2a296d82ffd8
Abdullah <habar@protonmail.com>
2023-05-28 11:07:48 +0200

    Gallery uses masonry and overlay

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,25 +1,38 @@
 <template>
     <main>
-        <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-            <div v-for="image in links" :key="image.original" :class="{ 'overlay': isHidden}">
+        <div class="columns-2 md:columns-3 lg:columns-4">
+            <div
+                v-for="image in links"
+                :key="image.original"
+                class="before:content-[''] before:rounded-md before:absolute before:inset-0 before:bg-black before:bg-opacity-20"
+                :class="{
+                    overlay: isHidden,
+                    'relative mb-4  before:content-[``] before:rounded-md before:absolute before:inset-0 before:bg-black before:bg-opacity-20': true,
+                }"
+            >
                 <img
-                    class="h-auto max-w-full rounded-lg"
+                    class="w-full rounded-md"
                     :src="image.original"
                     :alt="image.alt"
                     loading="lazy"
                     :width="image.width"
                     :height="image.height"
                 />
+                <div class="absolute inset-0 p-8 text-pink-300 flex flex-col z-50">
+                    <div class="relative text-ellipsis overflow-hidden">
+                        <h1 class="text-lg font-bold mb-3 ">{{ image.alt }}</h1>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
 </template>
 <script lang="ts">
 interface ImgSource {
-    original: string;
-    alt: string;
-    width: number;
-    height: number;
+    original: string
+    alt: string
+    width: number
+    height: number
 }
 export default {
     name: "Gallery",
@@ -30,25 +43,24 @@ export default {
             default: () => [],
         },
         isHidden: {
-          required: false,
-          type: Boolean,
-          default: false,
-      },
+            required: false,
+            type: Boolean,
+            default: false,
+        },
     },
 }
 </script>
 <style>
 .overlay {
-  position:relative;
+    position: relative;
 }
 .overlay:before {
-  content:"";
-  position: absolute;
-  top:5%;
-  left:5%;
-  height:90%;
-  width:90%;
-  background: rgba(255, 255, 255, 0.98);
-  z-index:999;
+    content: "";
+    position: absolute;
+    top: 5%;
+    left: 5%;
+    height: 90%;
+    width: 90%;
+    background: rgba(255, 255, 255, 0.98);
 }
 </style>
